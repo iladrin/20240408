405194dcc0893f38bc57f3f1e4d2cee6390a5196
Benoit Tavernier <benoit.tavernier@gmail.com>
2024-04-09 13:02:37 +0200

    New basic router

--- public/index.php
+++ public/index.php
@@ -2,9 +2,28 @@
 
 require '../vendor/autoload.php';
 
-$controller = 'user/list';
-
 const TEMPLATES_PATH = __DIR__ . '/../app/templates';
 
+//////////
+// Routing
+// On cherche à déterminer le controller associé à notre URL
+// Exemple d’URL : http://localhost/formation/index.php?page=users
+
+// Analyse de la Query string HTTP
+$page = $_GET['page'] ?? 'homepage';
+//$page = !empty($_GET['page']) ? $_GET['page'] : 'homepage';
+
+// Mapping de routes
+// Exemple : http://localhost/formation/?page=users => controller: user/list
+$routes = [
+    'homepage' => 'homepage',
+    'users' => 'user/list',
+];
+
+if (!isset($routes[$page])) {
+    trigger_error('Le controller demandé n\'existe pas', E_USER_ERROR);
+}
+
+$controller = $routes[$page];
 
 require "../app/controllers/$controller.php";
