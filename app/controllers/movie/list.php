48e256f2c6d758481310ace5ea795c7cf01a5a58
Benoit Tavernier <benoit.tavernier@gmail.com>
2024-04-10 15:24:31 +0200

    app

--- app/controllers/movie/list.php
+++ app/controllers/movie/list.php
@@ -7,18 +7,18 @@ writeLog('Movies loaded from YAML file', LOG_LEVEL_DEBUG);
 
 require_once '../app/services/templates.php';
 
-if (($file = @fopen('../app/data/movies.csv', 'r')) === false) {
-    trigger_error('Unable to open movies file', E_USER_ERROR);
-}
-
-while (($data = fgetcsv($file)) !== false) {
-    $movie = [
-        'id' => $data[0],
-        'title' => $data[1],
-        'synopsis' => $data[2],
-    ];
-
-    $movies[] = $movie;
-}
+//if (($file = @fopen('../app/data/movies.csv', 'r')) === false) {
+//    trigger_error('Unable to open movies file', E_USER_ERROR);
+//}
+//
+//while (($data = fgetcsv($file)) !== false) {
+//    $movie = [
+//        'id' => $data[0],
+//        'title' => $data[1],
+//        'synopsis' => $data[2],
+//    ];
+//
+//    $movies[] = $movie;
+//}
 
 renderTemplate('movie/list.php', ['movies' => $movies]);
