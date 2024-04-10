127bc2342cf19bfc61bd33685cacff0037df6a50
Benoit Tavernier <benoit.tavernier@gmail.com>
2024-04-10 17:23:48 +0200

    Update style to get a detter display for errors

--- app/templates/security/login.php
+++ app/templates/security/login.php
@@ -1,16 +1,17 @@
 <main class="w-1/2 mx-auto my-20 bg-emerald-800 text-white min-h-[300px] p-10">
   <h1 class="bloc mb-10 text-lg">Espace hautement sécurisé</h1>
 
-    <?php if (!empty($data['errors'])) : ?>
-      <ul>
-          <?php foreach ($data['errors'] as $error) : ?>
-            <li><?php echo $error ?></li>
-          <?php endforeach; ?>
-      </ul>
-    <?php endif; ?>
-
   <form action="" method="post"
         class="flex flex-col justify-center gap gap-3 w-3/4 m-auto">
+
+      <?php if (!empty($data['errors'])) : ?>
+        <ul class="text-orange-300 font-bold py-3">
+            <?php foreach ($data['errors'] as $error) : ?>
+              <li><?php echo $error ?></li>
+            <?php endforeach; ?>
+        </ul>
+      <?php endif; ?>
+
     <input type="text" name="username" placeholder="Nom d’utilisateur"
            class="block p-3 text-black rounded-md"/>
     <input type="password" name="password" placeholder="Mot de passe"
