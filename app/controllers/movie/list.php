993b715c2f87d1c66e73d292988c8013a15e44af
Benoit Tavernier <benoit.tavernier@gmail.com>
2024-04-10 14:22:07 +0200

    Use a YAML file instead of a CSV

--- app/controllers/movie/list.php
+++ app/controllers/movie/list.php
@@ -1,20 +1,23 @@
 <?php
 
+use Symfony\Component\Yaml\Yaml;
+
+$movies = Yaml::parseFile('../app/data/movies.yaml')['movies'];
+
 require_once '../app/services/templates.php';
 
-$movies = [];
-if (($file = @fopen('../app/data/movies.csv', 'r')) === false) {
-    trigger_error('Unable to open movies file', E_USER_ERROR);
-}
-
-while (($data = fgetcsv($file)) !== false) {
-    $movie = [
-        'id' => $data[0],
-        'title' => $data[1],
-        'synopsis' => $data[2],
-    ];
-
-    $movies[] = $movie;
-}
+//if (($file = @fopen('../app/data/movies.csv', 'r')) === false) {
+//    trigger_error('Unable to open movies file', E_USER_ERROR);
+//}
+//
+//while (($data = fgetcsv($file)) !== false) {
+//    $movie = [
+//        'id' => $data[0],
+//        'title' => $data[1],
+//        'synopsis' => $data[2],
+//    ];
+//
+//    $movies[] = $movie;
+//}
 
 renderTemplate('movie/list.php', ['movies' => $movies]);
